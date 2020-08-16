1d388d771d808a99cda7306668f8ce48552cb7dc
OlimoGD <69761323+OlimoGD@users.noreply.github.com>
2020-08-16 19:29:33 +0200

    basic moving and sprites

---
+++ Good Boi/Assets/Scripts/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.A)){
+            transform.position += Vector3.left;
+            spriteRenderer.flipX = true;
+        }else if(Input.GetKeyDown(KeyCode.W)){
+            transform.position += Vector3.up;
+        }else if(Input.GetKeyDown(KeyCode.D)){
+            transform.position += Vector3.right;
+            spriteRenderer.flipX = false;
+        }else if(Input.GetKeyDown(KeyCode.S)){
+            transform.position += Vector3.down;
+        }
+
+        if(Input.GetKeyDown(KeyCode.E)){
+            bark();
+        }
+    }
+
+    private void bark(){
+        
+    }
+}
