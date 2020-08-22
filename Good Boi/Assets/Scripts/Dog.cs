a41a9f81c1338e7e051c1a00dc513a2491dec58c
OlimoGD <69761323+OlimoGD@users.noreply.github.com>
2020-08-22 12:12:48 +0200

    enhanced movement + barking
    movement takes colliders into account

--- Good Boi/Assets/Scripts/Dog.cs
+++ Good Boi/Assets/Scripts/Dog.cs
@@ -6,26 +6,62 @@ public class Dog : MonoBehaviour
 {
     [SerializeField]
     private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip[] sounds;
+
+    private static readonly Vector3 spriteOffset = new Vector3(0.5f, 0.5f, 0f);
 
     private void Update(){
         if(Input.GetKeyDown(KeyCode.A)){
-            transform.position += Vector3.left;
+            Vector2 newPosition = getPosition() + Vector3.left;
+            moveTo(newPosition);
             spriteRenderer.flipX = true;
         }else if(Input.GetKeyDown(KeyCode.W)){
-            transform.position += Vector3.up;
+            Vector2 newPosition = getPosition() + Vector3.up;
+            moveTo(newPosition);
         }else if(Input.GetKeyDown(KeyCode.D)){
-            transform.position += Vector3.right;
+            Vector2 newPosition = getPosition() + Vector3.right;
+            moveTo(newPosition);
             spriteRenderer.flipX = false;
         }else if(Input.GetKeyDown(KeyCode.S)){
-            transform.position += Vector3.down;
+            Vector2 newPosition = getPosition() + Vector3.down;
+            moveTo(newPosition);
         }
 
-        if(Input.GetKeyDown(KeyCode.E)){
+        if(Input.GetKeyDown(KeyCode.Space)){
             bark();
         }
     }
 
+    private Vector3 getPosition(){
+        return transform.position + spriteOffset;
+    }
+
+    private void moveTo(Vector2 position){
+        if(isTileEmpty(position)){
+            transform.position = position - new Vector2(spriteOffset.x, spriteOffset.y);
+        }
+    }
+
+    private bool isTileEmpty(Vector2 position){
+        Collider2D collider = Physics2D.OverlapPoint(position);
+        return (collider == null);
+    }
+
     private void bark(){
-        
+        playRandomBarkSound();
+    }
+
+    private void playRandomBarkSound(){
+        //0, 1 or 2
+        int randomNumber = Random.Range(0, 3);
+        playSound(sounds[randomNumber]);
+    }
+
+    private void playSound(AudioClip sound){
+        audioSource.clip = sound;
+        audioSource.Play();
     }
 }
