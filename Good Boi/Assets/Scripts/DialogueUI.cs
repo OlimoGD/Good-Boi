fbc2909aa241e4ec8f4ed8aa906e3aceec0a5bb9
OlimoGD <69761323+OlimoGD@users.noreply.github.com>
2020-08-23 17:44:42 +0200

    dialogue system

---
+++ Good Boi/Assets/Scripts/DialogueUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text nameText;
+    [SerializeField]
+    private Text sentenceText;
+
+    public void setNameText(string name){
+        nameText.text = name;
+    }
+
+    public void setSentenceText(string sentence){
+        sentenceText.text = sentence;
+    }
+}
